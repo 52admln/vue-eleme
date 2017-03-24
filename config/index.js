3e413fef40f99046b83aa507baeb4b9a1834b69b
52admln <ismrwang@gmail.com>
2017-03-24 14:36:46 +0800

    express server

--- config/index.js
+++ config/index.js
@@ -3,11 +3,14 @@ var path = require('path')
 
 module.exports = {
   build: {
+    // 增加一个端口号
+    port: 9000,
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    // 生产环境不能让别人调试，将此置为 false
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
